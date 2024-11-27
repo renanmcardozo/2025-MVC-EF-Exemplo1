a9e1393a1a3748c0fb61ff4674ea250246dba88e
Daniel Corrêa Lobato <daniel@lobato.org>
2024-11-27 17:34:16 -0300

    Conclusão dos modelos e classes POCO

---
+++ Models/Autor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MVC_EF.Exemplo1.Models;
+
+public class Autor
+{
+    public int AutorID { get; set; }
+    public string AutorNome { get; set; }
+    public DateOnly? AutorDataNascimento { get; set; }
+    public string? AutorEmail { get; set; }
+
+    public ICollection<AutorLivro>? LivrosDoAutor { get; set; }
+}
+
+public class AutorConfiguration : IEntityTypeConfiguration<Autor>
+{
+    public void Configure(EntityTypeBuilder<Autor> builder)
+    {
+        builder.HasKey(p => p.AutorID);
+        
+        builder.HasIndex(p => p.AutorNome);
+        
+        builder.Property(p => p.AutorNome).HasMaxLength(80).IsRequired();
+        builder.Property(p => p.AutorEmail).HasMaxLength(80);
+        builder.Property(p => p.AutorDataNascimento)
+            .HasDefaultValue(DateOnly.FromDateTime(DateTime.Parse("1970/01/01")));
+    }
+}
--- Models/Livro.cs
+++ Models/Livro.cs
@@ -7,6 +7,14 @@ public class Livro
 {
     public int LivroID { get; set; }
     public string LivroTitulo { get; set; }
+    public ushort LivroPaginas { get; set; }
+    public string LivroISBN { get; set; }
+    public ushort LivroAnoPublicacao { get; set; }
+    public int EditoraID { get; set; }
+
+    public Editora EditoraDoLivro { get; set; }
+    public ICollection<OperacaoCompraVenda> OperacoesDoLivro { get; set; }
+    public ICollection<AutorLivro> AutoresDoLivro { get; set; }
 }
 
 public class LivroConfiguration : IEntityTypeConfiguration<Livro>
@@ -15,5 +23,10 @@ public class LivroConfiguration : IEntityTypeConfiguration<Livro>
     {
         builder.HasKey(p => p.LivroID);
         builder.Property(p => p.LivroTitulo).HasMaxLength(120).IsRequired();
+        builder.Property(p => p.LivroPaginas).HasDefaultValue(0).IsRequired();
+        builder.Property(p => p.LivroISBN).HasMaxLength(13).IsRequired();
+        
+        builder.HasOne<Editora>(p => p.EditoraDoLivro).WithMany(p => p.LivrosDaEditora);
+
     }
 }
