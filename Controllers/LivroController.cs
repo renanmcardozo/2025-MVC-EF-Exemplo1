5da841f907fe6878e0fb0557b41dac1ec6aee192
Daniel Corrêa Lobato <daniel@lobato.org>
2024-11-27 22:04:49 -0300

    Introdução do conceito de ViewModel Listagem de livros Edição de livros

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_EF.Exemplo1.Models;
 
 namespace MVC_EF.Exemplo1.Controllers;
 
@@ -22,7 +25,84 @@ public class LivroController : Controller
     // GET
     public IActionResult Index()
     {
-        var livros = _contexto.Livros;
-        return View();
+        var livros = _contexto.Livros
+            .Include(p => p.EditoraDoLivro)
+            .Include(p => p.AutoresDoLivro)
+            .ThenInclude(p => p.Autor);
+
+        var rset = new List<LivroEditoraAutorListViewModel>();
+        foreach (var livro in livros)
+        {
+            var r = new LivroEditoraAutorListViewModel();
+            r.Id = livro.LivroID;
+            r.Titulo = livro.LivroTitulo;
+            r.Ano = livro.LivroAnoPublicacao;
+            r.Paginas = livro.LivroPaginas;
+            r.ISBN = livro.LivroISBN;
+            r.Editora = livro.EditoraDoLivro.EditoraNome;
+            r.Autores = "";
+            foreach (var autor in livro.AutoresDoLivro)
+            {
+                r.Autores = r.Autores + autor.Autor.AutorNome + ", ";
+            }
+            rset.Add(r);
+        }
+        
+        return View(rset);
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        var livro = _contexto.Livros
+            .Include(p => p.EditoraDoLivro)
+            .Include(p => p.AutoresDoLivro)
+            .ThenInclude(p => p.Autor)
+            .FirstOrDefault(p=> p.LivroID == id);
+        if (livro == null)
+        {
+            return NotFound();
+        }
+
+        var livroViewModel = new LivroEditoraAutorEditViewModel();
+        livroViewModel.LivroID = livro.LivroID;
+        livroViewModel.Titulo = livro.LivroTitulo;
+        livroViewModel.Ano = livro.LivroAnoPublicacao;
+        livroViewModel.Paginas = livro.LivroPaginas;
+        livroViewModel.ISBN = livro.LivroISBN;
+        livroViewModel.EditoraID = livro.EditoraID;
+        livroViewModel.EditoraInputSelect = new SelectList(
+            _contexto.Editoras.OrderBy(p => p.EditoraNome).ToList(),
+            dataValueField: "EditoraID",
+            dataTextField: "EditoraNome");
+
+        return View(livroViewModel);
+    }
+
+    public IActionResult EditConfirm(int? id, LivroEditoraAutorEditViewModel livro)
+    {
+        if (id != livro.LivroID)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            var livroViewModel = new LivroEditoraAutorEditViewModel();
+            livroViewModel.LivroID = livro.LivroID;
+            livroViewModel.Titulo = livro.Titulo;
+            livroViewModel.Ano = livro.Ano;
+            livroViewModel.Paginas = livro.Paginas;
+            livroViewModel.ISBN = livro.ISBN;
+            livroViewModel.EditoraID = livro.EditoraID;
+            livroViewModel.EditoraInputSelect = new SelectList(
+                _contexto.Editoras.OrderBy(p => p.EditoraNome).ToList(),
+                dataValueField: "EditoraID",
+                dataTextField: "EditoraNome");
+
+            return View("Edit", livroViewModel);
+        }
+
+        // Alterar os dados
+        return Redirect($"Livro/Index/");
     }
 }
