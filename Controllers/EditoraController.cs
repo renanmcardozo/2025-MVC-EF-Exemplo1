3c37b151bedf46a9e2ef38f3b43c2677864507a0
Daniel Correa Lobato <daniel@lobato.org>
2024-12-11 14:19:21 -0300

    Mudança de layout e uso de Bootstrap Icons

---
+++ Controllers/EditoraController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_EF.Exemplo1.Controllers;
+
+public class EditoraController : Controller
+{
+    public IActionResult Index()
+    {
+        throw new NotImplementedException();
+    }
+}
