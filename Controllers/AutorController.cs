f47b658cd393e865c665528681e1008cea17ddb2
renanmcardozo <renanmcardozo@yahoo.com.br>
2024-12-18 13:00:17 -0300

    Segundo envio

--- Controllers/AutorController.cs
+++ Controllers/AutorController.cs
@@ -1,11 +1,109 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_EF.Exemplo1.Models;
+using MVC_EF.Exemplo1;
 
-namespace MVC_EF.Exemplo1.Controllers;
-
-public class AutorController : Controller
+namespace MVC_EF.Exemplo1.Controllers
 {
-    public IActionResult Index()
+    public class AutorController : Controller
     {
-        throw new NotImplementedException();
+        private readonly ApplicationDbContext _context;
+        
+        public AutorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var autores = await _context.Autores.ToListAsync();
+            return View("~/Views/Autor/Index.cshtml", autores);
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AutorNome,AutorDataNascimento,AutorEmail")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(autor);
+                await _context.SaveChangesAsync(); 
+                return RedirectToAction(nameof(Index));
+            }
+            return View(autor);
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            var autor = _context.Autores.Find(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AutorID, AutorNome, AutorDataNascimento, AutorEmail")] Autor autor)
+        {
+            if (id != autor.AutorID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(autor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Autores.Any(e => e.AutorID == autor.AutorID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(autor);
+        }
+
+// Ação Delete para exibir a confirmação de exclusão
+        public IActionResult Delete(int id)
+        {
+            var autor = _context.Autores.Find(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
+        }
+
+// Ação Delete (POST) para realmente excluir o autor
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var autor = await _context.Autores.FindAsync(id);
+            if (autor != null)
+            {
+                _context.Autores.Remove(autor);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- Controllers/EditoraController.cs
+++ Controllers/EditoraController.cs
@@ -2,44 +2,45 @@
 using Microsoft.EntityFrameworkCore;
 using MVC_EF.Exemplo1.Models;
 
-namespace MVC_EF.Exemplo1.Controllers
+namespace MVC_EF.Exemplo1.Controllers;
+
+public class EditoraController : Controller
 {
-    public class EditoraController : Controller
+    private readonly ApplicationDbContext _contexto;
+    private readonly int _pagesize;
+
+    public EditoraController(ApplicationDbContext contexto, IConfiguration configuration)
     {
-        private readonly ApplicationDbContext _contexto;
-        private readonly int _pagesize;
+        _contexto = contexto;
+        var pagesize = Convert.ToInt32(configuration
+            .GetSection("ViewOptions")
+            .GetSection("PageSize").Value);
+        _pagesize = pagesize > 0 ? pagesize : 10;
+    }
 
-        public EditoraController(ApplicationDbContext contexto, IConfiguration configuration)
+    // Corrigido: Implementação do método Index
+    public IActionResult Index()
+    {
+        // Busca todas as editoras no banco de dados
+        var editoras = _contexto.Editoras.ToList();
+
+        // Passa os dados para a View
+        return View(editoras);
+    }
+
+    public IActionResult Create()
+    {
+        var novo = new EditoraEditViewModel();
+
+        return View(novo);
+    }
+
+    [HttpPost, ActionName("Create")]
+    public IActionResult CreatePost(EditoraEditViewModel editora)
+    {
+        if (!ModelState.IsValid)
         {
-            _contexto = contexto;
-            var pagesize = Convert.ToInt32(configuration.
-                GetSection("ViewOptions").
-                GetSection("PageSize").Value);
-            _pagesize = pagesize > 0 ? pagesize : 10;
-        }
-
-        // Método Index: Lista as editoras
-        public IActionResult Index()
-        {
-            var editoras = _contexto.Editoras.ToList();
-            return View(editoras);
-        }
-
-        public IActionResult Create()
-        {
-            var novo = new EditoraEditViewModel();
-            return View(novo);
-        }
-
-        [HttpPost, ActionName("Create")]
-        public IActionResult CreatePost(EditoraEditViewModel editora)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(editora);
-            }
-
-            var novaEditora = new Editora
+            var novo = new EditoraEditViewModel
             {
                 EditoraNome = editora.EditoraNome,
                 EditoraLogradouro = editora.EditoraLogradouro,
@@ -52,18 +53,34 @@ namespace MVC_EF.Exemplo1.Controllers
                 EditoraTelefone = editora.EditoraTelefone
             };
 
-            try
-            {
-                _contexto.Editoras.Add(novaEditora);
-                _contexto.SaveChanges();
-                return RedirectToAction(nameof(Index)); // Redireciona para a lista após salvar
-            }
-            catch (DbUpdateException e)
-            {
-                ModelState.AddModelError("", "Erro ao salvar a editora. Tente novamente.");
-                Console.WriteLine(e);
-                return View(editora);
-            }
+            return View(novo);
         }
+
+        var novaEditora = new Editora
+        {
+            EditoraNome = editora.EditoraNome,
+            EditoraLogradouro = editora.EditoraLogradouro,
+            EditoraNumero = editora.EditoraNumero,
+            EditoraComplemento = editora.EditoraComplemento,
+            EditoraCidade = editora.EditoraCidade,
+            EditoraUF = editora.EditoraUF,
+            EditoraPais = editora.EditoraPais,
+            EditoraCEP = editora.EditoraCEP,
+            EditoraTelefone = editora.EditoraTelefone
+        };
+
+        _contexto.Editoras.Add(novaEditora);
+
+        try
+        {
+            _contexto.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return RedirectToAction(nameof(Index));
     }
 }
